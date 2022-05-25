a241124edb26e3ac53366b801fd1b72bc9004514
Justin Galang <justingalang@gmail.com>
2022-05-25 16:47:47 -0500

    initial app setup

---
+++ chooz-mobile/App/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Provider as PaperProvider } from "react-native-paper";
+import { Provider as StoreProvider } from "react-redux"; //TODO: Wrap App in Store Provider
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+let App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
